3e12219ef621c6832f9ec8037e84ab21c2bbc22b
Pavel Vorona <vorona.pael.nickolaevich@gmail.com>
2016-06-09 23:58:09 +0200

    remove mocks import

--- app/index.js
+++ app/index.js
@@ -2,6 +2,4 @@ import angular from 'angular';
 import router from 'angular-ui-router';
 import example from './src/views/example';
 
-import 'angular-mocks/angular-mocks';
-
 angular.module('smg.app', [router, example]);
